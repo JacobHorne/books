b324f3ae6a992aba72f11a53d622f880f790c633
jacobhorne@gmail.com <jacobhorne@gmail.com>
2018-12-11 01:32:13 -0500

    adding in logic

--- my-app/src/SearchArea.js
+++ my-app/src/SearchArea.js
@@ -5,11 +5,18 @@ const SearchArea = (props) => {
   return(
     <div className="searcharea">
       <form onSubmit={props.searchBooks} action="">
-        <input onChange={props.handleSearch} type="text"/>
+        <input onChange={props.handleSearch} placeholder='Search Books' type="text"/>
         <button type="submit" className="submitButton">Search</button>
       </form>
+      <div className="seperator">
+        <div className="seperator-text">
+          <span>booklist</span>
+          <h1>Most Popular</h1>
+          <p>Richland Library Wednesday, Dec 13</p>
+        </div>
+      </div>
     </div>
+    
   );
 }
-
 export default SearchArea;
